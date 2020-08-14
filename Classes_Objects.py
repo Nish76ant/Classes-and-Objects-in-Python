ece80670e18599d4e37c8d3668628032c734606b
Nishant kumar <55913847+Nish76ant@users.noreply.github.com>
2020-08-14 11:27:54 +0530

    Create Classes_Objects.py

---
+++ Classes_Objects.py
@@ -0,0 +1,31 @@
+#Classes and Objects 
+#OOPS -- Object Oriented Programming Language
+#Classes - Template 
+#Object - Instance of the Class
+
+#Dry - Do not repeat Yourself
+#if we us OOPS then we can save time ,money,effort
+
+#get_no_of_films(srk):
+
+class Student:
+
+
+    pass
+
+harry = Student() #Objects
+larry = Student() #Objects
+harry.name = 'Harry' #Instance
+harry.std = 12 #Instance 
+harry.section = 1 #Instance 
+harry.school = 'S.t Xavier School Jaipur' #Instance
+larry.std = 9
+larry.subjects = ['Chemistry','Physics','Mathes','Hindi','English'] 
+print(harry.name)
+print(harry.section)
+print(harry.std)
+print(harry.school)
+print(larry.std)
+print(larry.subjects)
+
+
